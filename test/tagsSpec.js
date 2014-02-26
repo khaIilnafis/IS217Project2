173e565da95320c492b8cd0080cee66650c9dab1
Khalil <knb6@njit.edu>
2014-02-26 00:31:12 -0500

    Unit Testing w/ Mocha & Chai Tutorial Halfway Done

--- test/tagsSpec.js
+++ test/tagsSpec.js
@@ -4,11 +4,25 @@ var tags = require("../lib/tags.js");
 
 describe("Tags", function(){
 	describe("#parse()", function(){
-		it("should parse long formed tags", function(){
+		it("should parse long formed tags and convert numbers", function(){
 			var args = ["--depth=4", "--hello=world"];
 			var results = tags.parse(args);
+
 			expect(results).to.have.a.property("depth", 4);
 			expect(results).to.have.a.property("hello", "world");
+		});
+		it("should fallback to defaults", function(){
+			var args = ["--depth=4", "--hello=world"];
+   		 	var defaults = { depth: 2, foo: "bar" };
+    		var results = tags.parse(args, defaults);
+ 
+    		var expected = {
+        		depth: 4,
+        		foo: "bar",
+        		hello: "world"
+    		};
+ 
+    		expect(results).to.deep.equal(expected);
 		});
 	});
 });
